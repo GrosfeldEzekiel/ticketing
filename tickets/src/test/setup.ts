bfd09c6d101c1b1c592891a5d2600f1e60dc0a28
Ezekiel Grosfeld <grosfeldezekiel@gmail.com>
2021-03-31 23:12:41 -0300

    Init tickets and moving to MongoAtlas

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,43 @@
+import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+export const getCookie = async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  return response.get("Set-Cookie");
+};
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "random";
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
