e816f684b2a4267bcf27c23bd21e1a5d0aecdddc
Ezekiel Grosfeld <grosfeldezekiel@gmail.com>
2021-05-19 01:53:40 -0300

    Final fix for client

--- client/pages/tickets/[id].tsx
+++ client/pages/tickets/[id].tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import https from 'https';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
@@ -63,14 +62,10 @@ export const getServerSideProps: GetServerSideProps = async ({
 	params,
 }) => {
 	try {
-		const agent = new https.Agent({
-			rejectUnauthorized: false,
-		});
 		const { data: ticket } = await axios.get(
 			`${process.env.API_URL}/api/tickets/${params.id}`,
 			{
 				headers: req.headers,
-				httpsAgent: agent,
 			}
 		);
 
